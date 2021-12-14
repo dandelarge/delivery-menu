f9d2fdf7c33d953509631e77f3673efeaaf9e574
Daniel Arauz <daniel@Daniels-MacBook-Pro-2.local>
2021-12-14 15:52:30 +0100

    que la verga

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 3012
+const port = process.env.PORT ||  3012;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
